cab21056f00fb50707882467ef6d82d751445e89
ahmed-nagah-s <elbatawya0@gmail.com>
2025-05-10 19:04:49 +0300

    Update of logical error in Upload Grades fun

--- CourseRegistrationV3/headerFiles/Admin.h
+++ CourseRegistrationV3/headerFiles/Admin.h
@@ -23,8 +23,22 @@ public:
     void loginAdmin();
     void addCourse(string code, string name, string instractor_name,
         string syllabus, float credit_hours);
+    void updateCourseCode(const string& oldCode, const string& newCode);
+    void updateCourseName(const string& code, const string& newName);
+    void updateInstructor(const string& code, const string& newInstructor);
+    void updateSyllabus(const string& code, const string& newSyllabus);
+    void updateCreditHours(const string& code, float newCreditHours);
+    void updateSemester(const string& code, int newSemester);
+
+    void updateCourseDetails(const string& code,
+        const string& newName = "",
+        const string& newInstructor = "",
+        const string& newSyllabus = "",
+        float newCreditHours = -1,
+        int newSemester = -1);
     void addPrerequisiteToCourse(const string& courseCode, const string& prerequisiteCode);
     void removePrerequisiteFromCourse(const string& courseCode, const string& prerequisiteCode);
+    void makeRegisteredCoursesAsCompleted(string id, vector<string> codes);
     void uploadGradesFromCsv(string& filePath);
     void updateGrades(string id, string code, int grade);
 };
