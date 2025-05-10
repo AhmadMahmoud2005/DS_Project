32b60a4dc9f79c833493eecf54fb064b1c6299f9
ahmed-nagah-s <elbatawya0@gmail.com>
2025-05-10 19:08:28 +0300

    Update of logical error in Upload Grades fun

--- CourseRegistrationV3/sourceFiles/Course.cpp
+++ CourseRegistrationV3/sourceFiles/Course.cpp
@@ -1,53 +1,80 @@
 #include "../headerFiles/Course.h"
 
 Course::Course() {
-   this->code = "";
-   this->name = "";
-   this->instractor_name = "";
-   this->syllabus = "";
-   this->credit_hours = 0.0;
-   this->semester = 1;
+    this->code = "";
+    this->name = "";
+    this->instractor_name = "";
+    this->syllabus = "";
+    this->credit_hours = 0.0;
+    this->semester = 1;
 }
 
 // Getters implementations
-string Course::getCode() const { return code; }
+string Course::getCode() const {
+    return code;
+}
 
-string Course::getName() const { return name; }
+string Course::getName() const {
+    return name;
+}
 
-string Course::getInstractorName() const { return instractor_name; }
+string Course::getInstractorName() const {
+    return instractor_name;
+}
 
-string Course::getSyllabus() const { return syllabus; }
+string Course::getSyllabus() const {
+    return syllabus;
+}
 
-int Course::getSemester() const { return semester; }
+int Course::getSemester() const {
+    return semester;
+}
 
-float Course::getCreditHours() const { return credit_hours; }
+float Course::getCreditHours() const {
+    return credit_hours;
+}
 
-const unordered_set<string> &Course::getPrerequisites() const {
-   return prerequisites;
+const unordered_set<string>& Course::getPrerequisites() const {
+    return prerequisites;
 }
 
 // Setters implementations
-void Course::setCode(const string &code) { this->code = code; }
-
-void Course::setName(const string &name) { this->name = name; }
-
-void Course::setInstractorName(const string &name) {
-   this->instractor_name = name;
+void Course::setCode(const string& code) {
+    this->code = code;
 }
 
-void Course::setSyllabus(const string &syllabus) { this->syllabus = syllabus; }
+void Course::setName(const string& name) {
+    this->name = name;
+}
 
-void Course::setSemester(int semester) { this->semester = semester; }
+void Course::setInstractorName(const string& name) {
+    this->instractor_name = name;
+}
 
-void Course::setCreditHours(float hours) { this->credit_hours = hours; }
+void Course::setSyllabus(const string& syllabus) {
+    this->syllabus = syllabus;
+}
 
-void Course::addPrerequisite(const string &code) { prerequisites.insert(code); }
+void Course::setSemester(int semester) {
+    this->semester = semester;
+}
 
-bool Course::removePrerequisite(const string &code) {
-   auto it = prerequisites.find(code);
-   if (it != prerequisites.end()) {
-      prerequisites.erase(it);
-      return true;
-   }
-   return false;
+void Course::setCreditHours(float hours) {
+    this->credit_hours = hours;
+}
+
+void Course::addPrerequisite(const string& code) {
+    prerequisites.insert(code);
+}
+bool Course::hasPrerequisite(const string& courseCode) const {
+    return prerequisites.find(courseCode) != prerequisites.end();
+}
+
+bool Course::removePrerequisite(const string& code) {
+    auto it = prerequisites.find(code);
+    if (it != prerequisites.end()) {
+        prerequisites.erase(it);
+        return true;
+    }
+    return false;
 }
